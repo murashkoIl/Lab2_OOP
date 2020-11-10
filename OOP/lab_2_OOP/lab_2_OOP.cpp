81963d2d16e5544819b2132ff69d19f706a93ec4
Ilya Murashko <muravey.ilya@mail.ru>
2020-11-10 23:17:51 +0300

    add namespace

--- OOP/lab_2_OOP/lab_2_OOP.cpp
+++ OOP/lab_2_OOP/lab_2_OOP.cpp
@@ -3,98 +3,106 @@
 
 using namespace std;
 
-class Animal
-{
-public:
-	virtual void cast_a_vote() {
-		cout << "Hrrrrr" << endl;
-	}
-	virtual void sight() {
-		cout << "Eyes look forward" << endl;
-	}
+namespace Animals {
 
-	// лапы
-	int paws;
-protected:
-	bool tail;
-	string teeth;
-};
+	class Animal
+	{
+	public:
+		virtual void cast_a_vote() {
+			cout << "Hrrrrr" << endl;
+		}
+		virtual void sight() {
+			cout << "Eyes look forward" << endl;
+		}
+		void set_teeth(string teeth) {
+			this->teeth = teeth;
+		}
+		string get_teeth() {
+			return this->teeth;
+		}
 
-class Food 
-{
-public:
-	virtual void type_of_food() {
-		cout << "Eat everything" << endl;
-	}
-};
+		// лапы
+		int paws;
+	protected:
+		bool tail;
+		string teeth;
+	};
 
-class PlantFood: public Food 
-{
-public:
-	void type_of_food() override {
-		cout << "Eat plants and grass" << endl;
-	}
-};
-class MeatFood : public Food
-{
-public:
-	void type_of_food() override {
-		cout << "Eats meat in most cases" << endl;
-	}
-};
-// хищник
-class Predator: virtual public Animal, public MeatFood {
-public:
-	Predator(bool tail) {
-		this->tail = tail;
-	}
-	bool get_tail() {
-		return tail;
-	}
-	// переместить методы с зубами в Animal 
-	void set_teeth(string teeth) {
-		this->teeth = teeth;
-	}
-	string get_teeth() {
-		return this->teeth;
-	}
-};
+	class Food
+	{
+	public:
+		virtual void type_of_food() {
+			cout << "Eat everything" << endl;
+		}
+	};
 
-// травоядный 
-class Grass_feeding: virtual public Animal, public PlantFood { 
-public:
-	void sight() override {
-		cout << "Eyes look to the sides" << endl;
-	}
-	void cast_a_vote() override {
-		cout << "Frrr" << endl;
-	}
+	class PlantFood : public Food
+	{
+	public:
+		void type_of_food() override {
+			cout << "Eat plants and grass" << endl;
+		}
+	};
+	class MeatFood : public Food
+	{
+	public:
+		void type_of_food() override {
+			cout << "Eats meat in most cases" << endl;
+		}
+	};
+	// хищник
+	class Predator : virtual public Animal, public MeatFood {
+	public:
+		Predator(bool tail) {
+			this->tail = tail;
+		}
+		bool get_tail() {
+			return tail;
+		}
+	};
 
-};
+	// травоядный 
+	class Grass_feeding : virtual public Animal, public PlantFood {
+	public:
+		void sight() override {
+			cout << "Eyes look to the sides" << endl;
+		}
+		void cast_a_vote() override {
+			cout << "Frrr" << endl;
+		}
 
-// всеядный 
-class Omnivorous: public Animal, public Food { 
-public:
-	// соединение подачи голоса хищника и травоядного 
+	};
 
-};
+	// всеядный 
+	class Omnivorous : public Animal, public Food {
+	public:
+		void cast_a_vote() override {
+			cout << "Fhfhfhrfh" << endl;
+		}
 
+	};
+
+}
 
 
 int main()
 {
+	using namespace Animals;
 	Grass_feeding Zebra;
 	Zebra.type_of_food();
 	Zebra.paws = 4;
 	cout << Zebra.paws << endl;
 	Omnivorous Vasya;
 	Vasya.type_of_food();
+	Vasya.cast_a_vote();
 
-	Predator Lion(1);
+	Predator Lion(true);
 	Lion.type_of_food();
 	cout << Lion.get_tail() << endl;
 	Lion.set_teeth("sharp");
 	cout << Lion.get_teeth() << endl;
+
+	Predator predat = new Predator(true);
 
 
 
